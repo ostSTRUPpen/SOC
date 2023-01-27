4e2273c500f1669aa4561a55427ba49e3df89407
VV <vojvol@post.cz>
2023-01-27 14:11:08 +0100

    server-side cors url test2

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -2,7 +2,7 @@ const allowedOrigins: string[] = require("./allowedOrigins");
 
 const corsOptions: any = {
 	origin: (origin: string, callback: any) => {
-		if (allowedOrigins.indexOf(origin) !== -1) {
+		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
 			callback(null, true);
 		} else {
 			callback(
