6dc0bf069e2ef1e259822e2d8802631af35595ad
VV <vojvol@post.cz>
2023-01-27 14:40:43 +0100

    server-side spelling error in html fix

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,9 +1,5 @@
-/*const allowedOrigins: string[] =
+const allowedOrigins: string[] =
 	process.env.NODE_ENV === "development"
 		? ["http://localhost:3000", "http://localhost:3500", "undefined"]
 		: ["https://learning-triangle-soc-version-api.onrender.com"];
-*/
-const allowedOrigins: string[] = [
-	"https://learning-triangle-soc-version-api.onrender.com",
-];
 export = allowedOrigins;
