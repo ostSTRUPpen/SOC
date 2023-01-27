4b40b902e195f41d87172b468f67bd526ee4dd4a
VV <vojvol@post.cz>
2023-01-27 13:42:03 +0100

    server-side added correct cors url part5

--- server/dist/config/allowedOrigins.js
+++ server/dist/config/allowedOrigins.js
@@ -1,6 +1,5 @@
 "use strict";
-const allowedOrigins =
-	process.env.NODE_ENV === "development"
-		? ["http://localhost:3000", "http://localhost:3500", "undefined"]
-		: ["https://learning-triangle-soc-version-api.onrender.com"];
+const allowedOrigins = process.env.NODE_ENV === "development"
+    ? ["http://localhost:3000", "http://localhost:3500", "undefined"]
+    : ["https://learning-triangle-soc-version-api.onrender.com"];
 module.exports = allowedOrigins;
--- server/dist/config/corsOptions.js
+++ server/dist/config/corsOptions.js
@@ -1,17 +1,15 @@
 "use strict";
 const allowedOrigins = require("./allowedOrigins");
-const isInDevelopment = process.env.NODE_ENV === "development" ? true : false;
+
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 ||
-            (!origin && isInDevelopment)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
+	origin: (origin, callback) => {
+		if (allowedOrigins.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
+	optionsSuccessStatus: 200,
 };
 module.exports = corsOptions;
