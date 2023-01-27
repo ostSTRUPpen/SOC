1ac2cf085c72bfee8a4eeffb34d36db8207c0926
VV <vojvol@post.cz>
2023-01-27 13:22:08 +0100

    server-side added correct url to cors

--- server/dist/config/allowedOrigins.js
+++ server/dist/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 "use strict";
-const allowedOrigins = process.env.NODE_ENV === "development"
-    ? ["http://localhost:3000", "http://localhost:3500", "undefined"]
-    : ["https:// TODO"];
+const allowedOrigins =
+	process.env.NODE_ENV === "development"
+		? ["http://localhost:3000", "http://localhost:3500", "undefined"]
+		: ["https://learning-triangle-soc-version-api.onrender.com/"];
 module.exports = allowedOrigins;
