bf685c8ceab912b8192161787856d99ad770d9ec
ostSTRUPpen <62558669+ostSTRUPpen@users.noreply.github.com>
2023-01-27 13:50:00 +0100

    Delete server/dist directory

--- server/dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-const loginLimiter = require("../middleware/loginLimiter");
-router.route("/").post(loginLimiter, authController.login);
-router.route("/refresh").get(authController.refresh);
-router.route("/logout").post(authController.logout);
-module.exports = router;
